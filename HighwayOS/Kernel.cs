cacbc33c31103e4d995f2a7c191a47b1a3bede23
Guilherme Pilz Moreira <gui.pilz.2007@gmail.com>
2024-10-31 20:22:03 -0300

    Installed COSMOS

---
+++ HighwayOS/Kernel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace HighwayOS
+{
+    public class Kernel : Sys.Kernel
+    {
+
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Highway booted successfully.");
+            Console.WriteLine("This is a dev version!");
+            Console.WriteLine("This is the first version of HighwayOS. =D");
+            Console.WriteLine("Type a line of text to get it echoed back.");
+        }
+
+        protected override void Run()
+        {
+            Console.Write("Input: ");
+            var input = Console.ReadLine();
+            Console.Write("Text typed: ");
+            Console.WriteLine(input);
+        }
+    }
+}
