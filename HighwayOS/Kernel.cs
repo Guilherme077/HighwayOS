96e6b1a4aa3dc6cf618c6a43ae66c33179d6bc41
Guilherme Pilz Moreira <gui.pilz.2007@gmail.com>
2024-11-02 11:39:53 -0300

    Add mouse and an image

--- HighwayOS/Kernel.cs
+++ HighwayOS/Kernel.cs
@@ -1,27 +1,62 @@
-﻿using System;
+﻿using Cosmos.System.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Drawing;
 using Sys = Cosmos.System;
+using IL2CPU.API.Attribs;
+using Cosmos.Core.IOGroup;
+using Cosmos.System;
+using Cosmos.Core.Memory;
 
 namespace HighwayOS
 {
     public class Kernel : Sys.Kernel
     {
-
+        public static VBECanvas canvas = new VBECanvas(new Mode(1920, 1080, ColorDepth.ColorDepth32)); // Set graphic Mode
+        [ManifestResourceStream(ResourceName = "HighwayOS.imgTest.bmp")] public static byte[] imgTest;
+        public static Bitmap bmp = new Bitmap(imgTest);
+        [ManifestResourceStream(ResourceName = "HighwayOS.cursor.bmp")] public static byte[] cursor;
+        public static Bitmap curs =new Bitmap(cursor);
         protected override void BeforeRun()
         {
-            Console.WriteLine("Highway booted successfully.");
-            Console.WriteLine("This is a dev version!");
-            Console.WriteLine("This is the first version of HighwayOS. =D");
-            Console.WriteLine("Type a line of text to get it echoed back.");
+
+            MouseManager.ScreenWidth = 1920;
+            MouseManager.ScreenHeight = 1080;
+            MouseManager.X = 1920 / 2;
+            MouseManager.Y = 1080 / 2;
+            
+
+
+
+
         }
 
         protected override void Run()
         {
-            Console.Write("Input: ");
-            var input = Console.ReadLine();
-            Console.Write("Text typed: ");
-            Console.WriteLine(input);
+
+            canvas.DrawFilledRectangle(new Pen(Color.LightCyan), 0, 0, 1920, 1080);
+
+            canvas.DrawImage(bmp, 50, 50);
+
+            int MousePosX = (int)MouseManager.X;
+            int MousePosY = (int)MouseManager.Y;
+            if(MousePosX > 1920 - 3)
+            {
+                MousePosX = 1918;
+            }
+            if (MousePosY > 1080 - 3)
+            {
+                MousePosY = 1078;
+            }
+
+
+            canvas.DrawImageAlpha(curs, MousePosX, MousePosY);
+            Heap.Collect();
+
+            canvas.Display(); //Allways draw the screen
+
+            
         }
     }
 }
