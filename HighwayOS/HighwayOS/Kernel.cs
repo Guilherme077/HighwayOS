d9d0432f4d75d5939316f80b07a219adfd7af86d
Guilherme Pilz Moreira <gui.pilz.2007@gmail.com>
2024-11-10 21:07:59 -0300

    Basic Command Processor added

--- HighwayOS/HighwayOS/Kernel.cs
+++ HighwayOS/HighwayOS/Kernel.cs
@@ -1,24 +1,40 @@
-﻿using System;
+﻿using HighwayOS.System32.Terminal;
+using System;
 using System.Collections.Generic;
+using System.CommandLine;
 using System.Text;
 using Sys = Cosmos.System;
+
 
 namespace HighwayOS
 {
     public class Kernel : Sys.Kernel
     {
-
+        CmdProcessor CmdProcessor = new CmdProcessor();
         protected override void BeforeRun()
         {
-            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+            Console.Clear();
+            Console.WriteLine("Cosmos booted successfully.");
+            Console.WriteLine("Welcome to HighwayOS");
+            Console.WriteLine("Type a command!");
+
         }
 
         protected override void Run()
         {
-            Console.Write("Input: ");
-            var input = Console.ReadLine();
-            Console.Write("Text typed: ");
-            Console.WriteLine(input);
+            Console.Write("-> ");
+            String[] console = Console.ReadLine().Split(" ");
+            CmdProcessor.Process(console);
+            Console.WriteLine("");
         }
+
+        public static void Shutdown()
+        {
+            Sys.Power.Shutdown();
+        }
+
+
+
+
     }
 }
---
+++ HighwayOS/HighwayOS/System32/Terminal/CmdProcessor.cs
@@ -0,0 +1,66 @@
+﻿using Cosmos.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighwayOS.System32.Terminal
+{
+    internal class CmdProcessor
+    {
+        public void Process(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                return;
+            }
+
+            var command = args[0].ToLower();
+
+            switch (command)
+            {
+                case "shutdown":
+                    Kernel.Shutdown();
+                    break;
+                case "help":
+                    ShowHelp(args);
+                    break;
+                case "sysinfo":
+                    ShowSysInfo();
+                    break;
+                default:
+                    Console.WriteLine($"ERROR: '{command}' is not a valid command");
+                    Console.WriteLine($"Type 'help' for help =D");
+                    break;
+            }
+        }
+
+        private void ShowHelp(String[] args)
+        {
+            if(args.Length == 1) 
+            {
+                Console.WriteLine("HELP: List of main commands. To show all commands available, type 'help all'");
+                Console.WriteLine("  shutdown    - Shutdown the HighwayOS");
+                Console.WriteLine("  help        - Show this message");
+                Console.WriteLine("  sysinfo     - Show info about the PC");
+            }
+            else if (args[1] == "all")
+            {
+                Console.WriteLine("HELP: List of all commands.");
+                Console.WriteLine("  shutdown    - Shutdown the HighwayOS");
+                Console.WriteLine("  help        - Show this message");
+                Console.WriteLine("  sysinfo     - Show info about the PC");
+            }
+            
+        }
+        private void ShowSysInfo()
+        {
+            Console.WriteLine("System Information:");
+            Console.WriteLine($"Memory: {CPU.GetAmountOfRAM()} MB");
+            Console.WriteLine($"CPU: {CPU.GetCPUVendorName()}");
+            Console.WriteLine($"System: HighwayOS Alpha");
+        }
+
+    }
+}
